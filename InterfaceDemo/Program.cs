db358b399b8e8a6a40499eccba6b769188e9d561
Ahmet <57807015+ahmet-cigil@users.noreply.github.com>
2024-02-14 11:57:09 +0100

    Add project files.

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IRun> runners = new List<IRun>();
+            Person person = new Person();
+            Animal animal = new Animal();
+
+            runners.Add(person);
+            runners.Add(animal);
+
+            Console.ReadLine();
+        }
+
+        public class Person: IRun
+        {
+            public string Name { get; set; }
+
+            public void Run()
+            {
+                
+            }
+        }
+
+        public class Animal: IRun
+        {
+
+            public string Name { get; set; }
+
+            public void Run()
+            {
+                
+            }
+        }
+
+        public interface IRun
+        {
+            void Run();
+        }
+    }
+}
